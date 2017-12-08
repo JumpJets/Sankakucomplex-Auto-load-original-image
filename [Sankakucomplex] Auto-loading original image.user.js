8b15a99e18c2a032ae0f0f682d1fd3a06035c3ab
XCanG <xan813@mail.ru>
2017-12-08 22:50:58 +0500

    Adding userscript

---
+++ [Sankakucomplex] Auto-loading original image.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         [Sankakucomplex] Auto-loading original image
+// @icon         https://chan.sankakucomplex.com/favicon.png
+// @namespace    https://github.com/JumpJets/Sankakucomplex-Auto-load-original-image
+// @version      1.0
+// @description  Replace sample image to original.
+// @author       XCanG
+// @match        https://chan.sankakucomplex.com/post/show/*
+// @match        https://idol.sankakucomplex.com/post/show/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    let link = document.getElementById("highres");
+	if (typeof link !== 'undefined') {
+		let linkclick = document.getElementById("image-link");
+		linkclick.removeAttribute("href");
+		//1 linkclick.outerHTML = linkclick.outerHTML;
+		//2 let new_element = linkclick.cloneNode(true);
+		//2 linkclick.parentNode.replaceChild(new_element, linkclick);
+		let page_img = document.getElementById("image");
+		page_img.src = link.href;
+		document.getElementById("resized_notice").remove();
+	}
+})();
